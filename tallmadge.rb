e98d161467b545f7bb670b9895cecfe465149151
Kevin Lawver <kevin@lawver.net>
2026-08-15 12:31:10 -0400

    Initial commit

---
+++ tallmadge.rb
@@ -0,0 +1,45 @@
+class Tallmadge < Formula
+  desc "CLI manager for ~/.agents/ and AI coding harness extensions"
+  homepage "https://github.com/kplawver/tallmadge"
+  url "https://github.com/kplawver/tallmadge/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "<SHA256_OF_RELEASE_TARBALL>"
+  license "MIT"
+
+  depends_on "ruby"
+  uses_from_macos "git"
+
+  resource "rainbow" do
+    url "https://rubygems.org/downloads/rainbow-3.1.1.gem"
+    sha256 "<SHA256_OF_RAINBOW_GEM>"
+  end
+
+  resource "thor" do
+    url "https://rubygems.org/downloads/thor-1.5.0.gem"
+    sha256 "<SHA256_OF_THOR_GEM>"
+  end
+
+  def install
+    ENV["GEM_HOME"] = libexec
+
+    resources.each do |r|
+      r.verify_download_integrity(r.fetch)
+      system "gem", "install", r.cached_download,
+             "--no-document",
+             "--ignore-dependencies",
+             "--install-dir", libexec
+    end
+
+    system "gem", "build", "tallmadge.gemspec"
+    system "gem", "install", "tallmadge-#{version}.gem",
+           "--no-document",
+           "--ignore-dependencies",
+           "--install-dir", libexec
+
+    bin.install libexec/"bin/clpr"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    assert_match "clpr #{version}", shell_output("#{bin}/clpr version")
+  end
+end
